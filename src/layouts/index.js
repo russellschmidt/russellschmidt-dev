92ec47ea69c3a5d3b6618f88a56b1bfcd8777d5a
Russell Schmidt <russell.schmidt@me.com>
2018-02-09 20:45:05 -0800

    minor changes and you broke it

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+import Media from 'react-media';
 
-import Header from '../components/Header'
-import './index.css'
+import Header from '../components/Header';
+import './index.css';
+import "../styles/layout-override.css";
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -17,13 +20,61 @@ const TemplateWrapper = ({ children }) => (
     <Header />
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
+        margin: "0 auto",
+        maxWidth: 980,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: "100%"      }}
     >
-      {children()}
+      <Media query={{ maxWidth: 848 }}>
+        { matches=>
+          matches ? (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 1 }}>{children()}</div>
+            </div>
+          ) : (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 2.5, paddingRight: "30px" }}>
+                { children() }
+              </div>
+              <div style={{ flex: 1 }}>
+                <Sidebar
+                  title="Articles"
+                  description="Thoughts on Development, all written by me"
+                />
+                <Sidebar
+                  title="Portfolio"
+                  description="Projects blessed with my code"
+                />
+                <Sidebar
+                  title="About Me"
+                  description="Biography of Yours Truly"
+                />
+              </div>
+            </div>
+          )}
+      </Media>
     </div>
   </div>
 )
